600f7884483bf005f4b224f7ff068c0306200851
AB Rao <anirudhrao24@gmail.com>
2018-10-25 04:34:02 +0200

    done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,9 +1,21 @@
-function deleteItem(e){
-
+function deleteItem(){
+  var item = document.getElementById('product-info');
+  item.parentNode.removeChild(item);
 }
 
-function getPriceByProduct(itemNode){
-
+function getPriceByProduct(){
+  console.log("Unit Price is: " + parseFloat(document.getElementById('unit-price').innerText)) //Unit Price
+  console.log("Quantity:  " + document.getElementById("quantity").value)
+  console.log("Unit Price x Quantity:  " + totalUnitPrice)
+//  var totalQuantity = document.getElementById("quantity").value
+  var unitPrice = parseFloat(document.getElementById('unit-price').innerText);
+  var totalQuantity = document.getElementById("quantity").value;
+  var totalUnitPrice = unitPrice*totalQuantity;
+// var
+  //total-quantity-price, var productPrice = document.getElementById('total-quantity-price'),
+  //var num = parseFloat(productPrice.innerText)
+  // var unitPrice = parseFloat(document.getElementById('unit-price').innerText)
+  //
 }
 
 function updatePriceByProduct(productPrice, index){
